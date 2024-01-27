8a102a9ca3e18aac98b5b77d6cec839c53e96527
cafitac <cafitac99@gmail.com>
2024-01-27 20:06:10 +0900

    순환참조

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ class A:
     def __init__(self, name):
         print("init")
         self.name = name
+        self.parent = None
         self.children = set()
 
     def __del__(self):
@@ -14,7 +15,9 @@ class A:
 if __name__ == '__main__':
     a = A("1")
     b = A("2")
-    a.add(b)
+    b.parent = a
+    a.parent = b
+
     print("end")
 
 
@@ -24,6 +27,6 @@ print 결과
 init
 init
 end
-delete 1
-delete 2
+delete 1  # 순환 참조로 인해 __del__ 함수의 실행 여부가 보장되지 않음 (해당 코드에서는 호출된 모습)
+delete 2  # 순환 참조로 인해 __del__ 함수의 실행 여부가 보장되지 않음 (해당 코드에서는 호출된 모습)
 """
